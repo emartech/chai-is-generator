fb59069cb009c3ffe51ecca355b125412fa0c81d
Adam Brunner <brunner.adam@gmail.com>
2016-06-20 12:29:24 +0200

    feat(assert): use is-generator for testing subject

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
 
+var isGenerator = require('is-generator').fn;
+
 module.exports = function(chai) {
   var Assertion = chai.Assertion;
 
   Assertion.addProperty('generator', function() {
-    this.assert(this._obj.constructor.name === 'GeneratorFunction',
-      'expected #{this} to be a generator',
-      'expected #{this} not to be a generator'
-    );
+    this.assert(isGenerator(this._obj), 'expected #{this} to be a generator', 'expected #{this} not to be a generator');
   });
 
   chai.assert.isGenerator = function(value, msg) {
